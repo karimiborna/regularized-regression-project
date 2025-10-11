6067eec12e2651aa39d6a8d420e2aa028101859a
alanluo666 <alan.yuluo@gmail.com>
2025-10-10 21:09:47 -0700

    Update about.py

--- sections/about.py
+++ sections/about.py
@@ -2,8 +2,8 @@ from dash import html
 
 layout = html.Div([
     html.H2("About This Project"),
-    html.P("This dashboard was created for the Final Project in the Linear Regression Analysis class at USFCA. "
-           "It demonstrates how Ridge and Lasso regression apply penalties to coefficients "
+    html.P("This dashboard was for the Final Project of F25 Linear Regression Analysis class at USFCA."),
+        html.P("It demonstrates how Ridge and Lasso regression apply penalties to coefficients "
            "to reduce overfitting and improve generalization."),
     html.P("Author: Borna Karimi, Alan Luo"),
     html.P("Built with: Python, scikit-learn, Plotly Dash")
